9ddf36599baf9b828cf9f7ade618213169eb841f
Vladimir Sabev <vdsabev@gmail.com>
2017-02-23 09:55:26 +0200

    Working (but unstyled) version

--- src/app/room-list.ts
+++ src/app/room-list.ts
@@ -4,28 +4,13 @@ import Room from './room';
 import SupplyList from './supply-list';
 
 export default class RoomList extends Component<RoomList> {
-  private rooms: Room[] = [];
+  private rooms: Room[] = this.getRooms();
   private supplyList: SupplyList;
 
   render() {
     return [
       select({
         className: 'room-list-select',
-        oninit: () => {
-          this.rooms = [];
-          firebase.database().ref('rooms').on('child_added', (roomChildSnapshot: any) => {
-            const room = new Room(roomChildSnapshot.val());
-            room.id = roomChildSnapshot.key;
-            this.rooms.push(room);
-            if (!this.supplyList) {
-              this.setSupplyList(room);
-            }
-            this.update();
-          });
-        },
-        onremove: () => {
-          firebase.database().ref('rooms').off('child_added');
-        },
         onchange: ($event: Event) => {
           const value = (<HTMLSelectElement>$event.target).value;
           this.rooms.forEach((room) => {
@@ -43,15 +28,32 @@ export default class RoomList extends Component<RoomList> {
     ];
   }
 
+  private getRooms(): Room[] {
+    const rooms: Room[] = [];
+    const roomsRef = firebase.database().ref('rooms');
+    roomsRef.off('child_added');
+    roomsRef.on('child_added', (roomChildSnapshot: any) => {
+      const room = new Room(roomChildSnapshot.val());
+      room.id = roomChildSnapshot.key;
+      rooms.push(room);
+      if (!this.supplyList) {
+        this.setSupplyList(room);
+      }
+      this.update();
+    });
+
+    return rooms;
+  }
+
+  private setSupplyList(room: Room) {
+    this.supplyList = new SupplyList(this, { room });
+  }
+
   private renderSupplyList(supplyList: SupplyList): Mithril.VirtualElement {
     if (!(supplyList && supplyList.room)) return null;
 
     return div({ className: 'room-list-item' },
       supplyList.render()
     );
-  }
-
-  private setSupplyList(room: Room) {
-    this.supplyList = new SupplyList(this, { room });
   }
 }
