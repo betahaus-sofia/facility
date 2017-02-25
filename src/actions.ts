909562b0d7d75dcbd08cfd32d8cbf9e73b4980f2
Vladimir Sabev <vdsabev@gmail.com>
2017-02-26 01:45:56 +0200

    Unfinished version with redux

---
+++ src/actions.ts
@@ -0,0 +1,8 @@
+enum Actions {
+  ADD_ROOM,
+  SELECT_ROOM,
+  ADD_SUPPLY,
+  SUPPLY_REQUESTED
+}
+
+export default Actions;
--- src/room-list/index.ts
+++ src/room-list/index.ts
@@ -1,9 +1,7 @@
 import { select, option } from 'compote';
 
-import Room from '../room';
-import Supply from '../supply';
-
-import { render } from '../facility';
+import { Room } from '../room';
+import { getSupplies } from '../supply';
 
 export function RoomList(rooms: Room[]) {
   return (
@@ -14,7 +12,6 @@ export function RoomList(rooms: Room[]) {
         rooms.forEach((room) => {
           if (room.id === value) {
             getSupplies(rooms, room);
-            render(rooms, room);
           }
         });
       }
@@ -22,27 +19,4 @@ export function RoomList(rooms: Room[]) {
       option({ value: room.id }, room.name))
     ))
   );
-}
-
-export function getSupplies(rooms: Room[], room: Room) {
-  const supplies: Supply[] = [];
-  const suppliesRef = firebase.database().ref(`rooms/${room.id}/supplies`);
-  suppliesRef.off('child_added');
-  suppliesRef.on('child_added', (roomSupplyChildSnapshot: any) => {
-    firebase.database().ref(`supplies/${roomSupplyChildSnapshot.key}`).once('value', (supplySnapshot: any) => {
-      const supply = new Supply(supplySnapshot.val());
-      supply.id = supplySnapshot.key;
-      supplies.push(supply);
-      render(rooms, room, supplies);
-
-      // TODO: Unsubscribe on remove
-      firebase.database().ref(`roomSupplies/${room.id}_${supply.id}/requested`).on('value', (requestedSnapshot: any) => {
-        const requested = requestedSnapshot.val();
-        if (requested) {
-          supply.requested = Math.min(requested, Date.now());
-          render(rooms, room, supplies);
-        }
-      });
-    });
-  });
 }
