e3073cd3d8a8b663ed59ac96ab11570013934fbd
Vladimir Sabev <vdsabev@gmail.com>
2017-03-18 16:43:41 +0200

    Remove center circle and adjust size

--- src/clock/index.ts
+++ src/clock/index.ts
@@ -1,11 +1,11 @@
 import { svg, circle, line } from '../svg';
 
-const clockSize = 17;
+const clockSize = 18;
 const clockMargin = 3;
 const clockCenter = clockSize / 2;
 const clockRadius = clockSize / 2 - clockMargin;
-const hoursLineMultiplier = 0.5;
-const minutesLineMultiplier = 0.7;
+const hoursLineMultiplier = 0.4;
+const minutesLineMultiplier = 0.6;
 
 export const timeToXY = (max: number) => (minutes: number): [number, number] => {
   const multiplier = 2 * Math.PI * minutes / max;
@@ -35,15 +35,7 @@ export const Clock = (date: Date) => {
         style: {
           fill: 'none',
           stroke: 'white',
-          'stroke-width': 1.25
-        }
-      }),
-      circle({
-        cx: clockCenter,
-        cy: clockCenter,
-        r: 1,
-        style: {
-          fill: 'white'
+          'stroke-width': 2
         }
       }),
       line({
