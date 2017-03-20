ff0c7301abcbeeef556fd96d504fdb0cea15dd29
Vladimir Sabev <vdsabev@gmail.com>
2017-03-21 00:48:10 +0200

    Replaced background URL with image element

--- src/supply-list/index.ts
+++ src/supply-list/index.ts
@@ -1,6 +1,6 @@
 import './style.scss';
 
-import { div, h4 } from 'compote/html';
+import { div, img, h4 } from 'compote/html';
 const timeago = require('timeago.js');
 
 import { Clock } from '../clock';
@@ -21,11 +21,9 @@ export const SupplyRequestedDate = (requested: Date) => (
 export const SupplyListItem = (room: Room) => (supply: Supply) => (
   div({ key: supply.id, className: 'supply-list-item fade-in-animation' }, [
     div({ className: 'supply-list-item-button' }, [
-      div({
-        className: 'supply-list-item-container',
-        style: { 'background-image': `url(${supply.imageUrl || '/assets/icon.png'})` },
-        onclick: () => requestSupply(room, supply)
-      }),
+      div({ className: 'supply-list-item-container', onclick: () => requestSupply(room, supply) },
+        img({ src: supply.imageUrl || '/assets/icon.png' })
+      ),
       h4({ className: 'supply-list-item-name' }, supply.name),
       supply.requested ? SupplyRequestedDate(new Date(supply.requested)) : null
     ])
