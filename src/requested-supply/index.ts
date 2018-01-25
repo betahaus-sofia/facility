28db2bff749cc95b403b9436e100a66488f06e2a
Vladimir Sabev <vdsabev@gmail.com>
2018-01-25 23:48:48 +0200

    Fixed room suplly notified rule

--- src/requested-supply/index.ts
+++ src/requested-supply/index.ts
@@ -2,39 +2,34 @@ import { redraw } from 'mithril';
 import { div, h1 } from 'compote/html';
 import { getAnimationDuration } from 'compote/css';
 
-import { Actions, store } from '../store';
-import { Supply } from '../supply';
+import { setRequestedSupply, Supply } from '../supply';
 
-export const RequestedSupply = (supply: Supply) => (
-  div({
-    class: 'fixed stretch bg-success flex-row justify-content-center align-items-center',
-    oncreate({ dom }) {
-      dom.classList.add('fade-in-animation');
-      dom.children[0].classList.add('scale-in-animation');
-      setTimeout(() => {
-        setRequestedSupply(null);
-        redraw();
-      }, 1e3);
+export const RequestedSupply = (supply: Supply) =>
+  div(
+    {
+      class: 'fixed stretch bg-success flex-row justify-content-center align-items-center',
+      oncreate({ dom }) {
+        dom.classList.add('fade-in-animation');
+        dom.children[0].classList.add('scale-in-animation');
+        setTimeout(() => {
+          setRequestedSupply(null);
+          redraw();
+        }, 1e3);
+      },
+      onbeforeremove({ dom }) {
+        dom.classList.remove('fade-in-animation');
+        dom.children[0].classList.remove('scale-in-animation');
+
+        dom.classList.add('fade-out-animation');
+        dom.children[0].classList.add('scale-out-animation');
+
+        return new Promise((resolve) => {
+          setTimeout(resolve, 0.95 * getAnimationDuration(dom) * 1e3);
+        }).then(() => {
+          dom.classList.remove('fade-out-animation');
+          dom.children[0].classList.remove('scale-out-animation');
+        });
+      },
     },
-    onbeforeremove({ dom }) {
-      dom.classList.remove('fade-in-animation');
-      dom.children[0].classList.remove('scale-in-animation');
-
-      dom.classList.add('fade-out-animation');
-      dom.children[0].classList.add('scale-out-animation');
-
-      return new Promise((resolve) => {
-        setTimeout(resolve, 0.95 * getAnimationDuration(dom) * 1e3);
-      }).then(() => {
-        dom.classList.remove('fade-out-animation');
-        dom.children[0].classList.remove('scale-out-animation');
-      });
-    }
-  },
     h1({ class: 'text-center' }, `${supply.name} requested!`)
-  )
-);
-
-const setRequestedSupply = (supply: Supply) => {
-  store.dispatch({ type: Actions.SET_REQUESTED_SUPPLY, supply });
-};
+  );
--- src/supply/index.ts
+++ src/supply/index.ts
@@ -14,27 +14,42 @@ export class Supply extends Model<Supply> {
 
 export const unsubscribeFromSupplies = (room: Room, supplies: Supply[]) => {
   if (room) {
-    firebase.database().ref(`rooms/${room.id}/supplies`).off('child_added');
+    firebase
+      .database()
+      .ref(`rooms/${room.id}/supplies`)
+      .off('child_added');
     supplies.forEach((supply) => {
-      firebase.database().ref(`roomSupplies/${room.id}_${supply.id}/requested`).off('value');
+      firebase
+        .database()
+        .ref(`roomSupplies/${room.id}_${supply.id}/requested`)
+        .off('value');
     });
   }
 };
 
 export const getSupplies = (room: Room) => {
-  firebase.database().ref(`rooms/${room.id}/supplies`).on('child_added', (roomSupplyChildSnapshot: DataSnapshot<any>) => {
-    firebase.database().ref(`supplies/${roomSupplyChildSnapshot.key}`).once('value', (supplySnapshot: DataSnapshot<Supply>) => {
-      const supply = new Supply({ id: supplySnapshot.key }, supplySnapshot.val());
-      addSupply(supply);
+  firebase
+    .database()
+    .ref(`rooms/${room.id}/supplies`)
+    .on('child_added', (roomSupplyChildSnapshot: DataSnapshot<any>) => {
+      firebase
+        .database()
+        .ref(`supplies/${roomSupplyChildSnapshot.key}`)
+        .once('value', (supplySnapshot: DataSnapshot<Supply>) => {
+          const supply = new Supply({ id: supplySnapshot.key }, supplySnapshot.val());
+          addSupply(supply);
 
-      firebase.database().ref(`roomSupplies/${room.id}_${supply.id}/requested`).on('value', (requestedSnapshot: DataSnapshot<number>) => {
-        const requested = requestedSnapshot.val();
-        if (requested) {
-          supplyRequested(supply, Math.min(requested, Date.now()));
-        }
-      });
+          firebase
+            .database()
+            .ref(`roomSupplies/${room.id}_${supply.id}/requested`)
+            .on('value', (requestedSnapshot: DataSnapshot<number>) => {
+              const requested = requestedSnapshot.val();
+              if (requested) {
+                supplyRequested(supply, Math.min(requested, Date.now()));
+              }
+            });
+        });
     });
-  });
 };
 
 const addSupply = (supply: Supply) => {
@@ -42,11 +57,14 @@ const addSupply = (supply: Supply) => {
 };
 
 export const requestSupply = (room: Room, supply: Supply) => {
-  firebase.database().ref(`roomSupplies/${room.id}_${supply.id}`).update({
-    room: room.id,
-    supply: supply.id,
-    requested: firebase.database.ServerValue.TIMESTAMP
-  });
+  firebase
+    .database()
+    .ref(`roomSupplies/${room.id}_${supply.id}`)
+    .update({
+      room: room.id,
+      supply: supply.id,
+      requested: firebase.database.ServerValue.TIMESTAMP,
+    });
 
   supplyRequested(supply, Date.now());
   setRequestedSupply(supply);
@@ -56,6 +74,6 @@ const supplyRequested = (supply: Supply, requested: number) => {
   store.dispatch({ type: Actions.SUPPLY_REQUESTED, supply, requested });
 };
 
-const setRequestedSupply = (supply: Supply) => {
+export const setRequestedSupply = (supply: Supply) => {
   store.dispatch({ type: Actions.SET_REQUESTED_SUPPLY, supply });
 };
